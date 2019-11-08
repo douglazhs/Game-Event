accbaa49f3a0a9b548e1221bd0b8a8929ccdc071
douglazhs <douglashenrike.dsp@gmail.com>
2019-11-08 11:14:15 -0300

    Update

--- game_event.c
+++ game_event.c
@@ -1,11 +1,14 @@
 //Programa de controle de stands de jogos em um evento de video games.
-//Autores: Douglas Henrique de Souza Pereira|Joรฃo Victor de Souza Portella.
-//Matrรญculas: UC19107076|UC19100100.
-//Instituiรงรฃo de Ensino: Universidade Catรณlica de Brasรญlia.
+//Autores: Douglas Henrique de Souza Pereira|Joรยฃo Victor de Souza Portella.
+//Matrรยญculas: UC19107076|UC19100100.
+//Instituiรยงรยฃo de Ensino: Universidade Catรยณlica de Brasรยญlia.
 //Data: 02 de Novembro de 2019.
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
+
+#define ESC '27'
 
 typedef struct{
 	int totalRPG;
@@ -24,7 +27,7 @@ typedef struct{
 
 typedef struct{
 	char nome[100];
-	char tipo_stand;
+	char stand;
 	char email[150];
 }gamers;
 
@@ -37,7 +40,7 @@ typedef struct{
 void menu_participantes();
 void menu_organizadores();
 void menu_patrocinadores();
-int cad_empresas();
+void cad_empresas();
 void cad_participantes(Stands);
 void cad_patrocinadores();
 void stands(Stands);
@@ -45,34 +48,40 @@ void backup();
 void restore();
 void vagas();
 void sobre();
-void leValidaEmail();
+void acessar();
 
 int main(void){
 	char escolha;
+	
+	system("cls");
 	do{
-		printf("\n\t\t\t\t\tUCBG - UNIVERSIDADE CATOLICA DE BRASILIA GAMES");
-		printf("");
-		printf("\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tO QUE VOCE E? [1]PARTICIPANTE/[2]EMPRESA/[3]PATROCINADOR");
-		printf("\n\t\t\t\t\t      APERTE A RESPECTIVA LETRA>> ");
+		printf("\n\t\t\t\t\t UCBG - UNIVERSIDADE CATOLICA DE BRASILIA GAMES");
+		printf("\n\n\n\n\n\n\n\n\n");
+		printf("\t\t\t\t\t\t\t%c O QUE VOCE E?\n", 254);
+		printf("\n\t\t\t\t\t\t\t%c [1]PARTICIPANTE", 254);
+		printf("\n\t\t\t\t\t\t\t%c [2]EMPRESA", 254);
+		printf("\n\t\t\t\t\t\t\t%c [3]PATROCINADOR", 254);
+		printf("\n\t\t\t\t\t\t\t%c [0]SAIR DO PROGRAMA", 254);
+		printf("\n\n\t\t\t\t\t          %c APERTE A RESPECTIVA OPCAO %c ", 254, 175);
 		escolha = getch();
 		escolha = toupper(escolha);
-		if(escolha != '1' && escolha != '2' && escolha != '3'){
-			printf("\n\t\t\t\t\t\t  CARACTER INVALIDO!");
+		if(escolha != '1' && escolha != '2' && escolha != '3' && escolha != '0'){
+			printf("\n\t\t\t\t\t\t\tCARACTER INVALIDO!");
 			Sleep(800);
 		}
 		system("cls");
-	}while(escolha != '1' && escolha != '2' && escolha != '3');
-	
+	}while(escolha != '1' && escolha != '2' && escolha != '3' && escolha != '0');
+
 	switch(escolha){
 		case '1':
 			menu_participantes();
 			break;
 		case '2':
-			menu_organizadores();	
+			menu_organizadores();
 			break;
 		case '3':
 			menu_patrocinadores();
-			break;	
+			break;
 	}
 }
 
@@ -82,18 +91,20 @@ void menu_participantes(){
 	do{
 		system("cls");
 		printf("\n\n\n\n\n\n\n");
-		printf("\n                                           MENU PARTICIPANTE\n");
-		printf("                                          ยณ                                   ยณ\n");
-		printf("                                          ยณ           #1[CADASTRAR]           ยณ\n");
-		printf("                                          ยณ           #2[STANDS]              ยณ\n");
-		printf("                                          ยณ           #3[SOBRE O EVENTO]      ยณ\n");
-		printf("                                          ยณ           #0[FECHAR]              ยณ\n");
-		printf("                                          ยณ                                   ยณ\n");
-		printf("                                          ");
-		printf("\n\t\t\t\t\t\t\t#OPCAO>> ");
+		printf("\n                                  ษอออออออออออออออออ MENU PARTICIPANTE อออออออออออออออ%c\n", 187);
+		printf("                                  บ                                                   บ\n");
+		printf("                                  บ                   %c 1[CADASTRAR]                  บ\n", 254);
+		printf("                                  บ                   %c 2[ACESSAR]                    บ\n", 254);
+		printf("                                  บ                   %c 3[STANDS]                     บ\n", 254);
+		printf("                                  บ                   %c 4[SOBRE O EVENTO]             บ\n", 254);
+		printf("                                  บ                   %c 5[VOLTAR AO INICIO]           บ\n", 254);
+		printf("                                  บ                   %c 0[FECHAR]                     บ\n", 254);
+		printf("                                  บ                                                   บ\n");
+		printf("                                  ศอออออออออออออออออออออออออออออออออออออออออออออออออออ%c", 188);
+		printf("\n\t\t\t\t\t\t\t%c OPCAO %c ", 254, 175);
 		opcao = getch();
-		if(opcao != '1' && opcao != '2' && opcao != '3' && opcao != '0'){
-			printf("\n\t\t\t\t\t\t    OPCAO INVALIDA!");
+		if(opcao != '1' && opcao != '2' && opcao != '3' && opcao != '4' && opcao != '5' &&opcao != '0'){
+			printf("\n\t\t\t\t\t\t      OPCAO INVALIDA!");
 			Sleep(800);
 		}
 		switch(opcao){
@@ -101,39 +112,59 @@ void menu_participantes(){
 				cad_participantes(tipo);
 				break;
 			case '2':
-				stands(tipo);
+				acessar();
 				break;
 			case '3':
+				stands(tipo);
+				break;
+			case '4':
 				sobre();
+			case '5':
+				main();
 				break;		
 		}
 	}while(opcao != '0');
 }
 
 void menu_organizadores(){
-	int opcao, tentativa;
+	int opcao, tentativa, flag;
+		char resp;
 	Stands total;
-	int senha = cad_empresas();
-	do{
-		printf("\nDIGITE A SENHA DE ACESSO>> ");
-		scanf("%d", &tentativa);
-		printf("\n\t\t\t\t\t\t    SENHA INVALIDA!");
-		Sleep(800);
-	}while(senha != tentativa);
+	FILE *arqEMP;
+	organizadores empresas;
+	
+	arqEMP = fopen("organizadores.dat", "ab+");
+	printf("\n\t%c DIGITE [S] SE JA E CADASTRADO E QUALQUER OUTRA TECLA SE NAO E %c ", 254, 175);
+	resp = getchar();
+	resp = toupper(resp);
+	if(resp == 'S'){
+		while(fread(&empresas, sizeof(empresas), 1, arqEMP) && flag == 1);
+			do{
+				printf("\n\t%c DIGITE A SENHA DE ACESSO %c ", 254, 175);
+				scanf("%d", &tentativa);
+				if(tentativa != empresas.senha){
+					printf("\n\t\t\t\t\t\t    SENHA INVALIDA!");
+					flag = 1;
+					Sleep(800);		
+				}
+			}while(empresas.senha != tentativa);
+	}else
+		cad_empresas();	
 	do{
 		system("cls");
 		printf("\n\n\n\n\n\n\n\n");
-		printf("\n                                          รรรรรรรรร MENU ORGANIZADOR รรรรรรรรยฟ\n");
-		printf("                                          ยณ                                  ยณ\n");
-		printf("                                          ยณ           #1[VAGAS]              ยณ\n");
-		printf("                                          ยณ           #2[BACKUP DE DADOS]    ยณ\n");
-		printf("                                          ยณ           #3[RECUPERAR DADOS]    ยณ\n");
-		printf("                                          ยณ           #0[FECHAR]             ยณ\n");
-		printf("                                          ยณ                                  ยณ\n");
-		printf("                                          ");
-		printf("\n\t\t\t\t\t\t\t#OPCAO>> ");
+		printf("\n                                  ษอออออออออออออออออ MENU ORGANIZADOR ออออออออออออออออ%c\n", 187);
+		printf("                                  บ                                                   บ\n");
+		printf("                                  บ                   %c 1[VAGAS]                      บ\n", 254);
+		printf("                                  บ                   %c 2[BACKUP DE DADOS]            บ\n", 254);
+		printf("                                  บ                   %c 3[RESTAURAR DADOS]            บ\n", 254);
+		printf("                                  บ                   %c 4[VOLTAR AO INICIO]           บ\n", 254);
+		printf("                                  บ                   %c 0[FECHAR]                     บ\n", 254);
+		printf("                                  บ                                                   บ\n");
+		printf("                                  ศอออออออออออออออออออออออออออออออออออออออออออออออออออ%c", 188);
+		printf("\n\t\t\t\t\t\t\t%c OPCAO %c", 254, 175);
 		opcao = getch();
-		if(opcao != '1' && opcao != '2' && opcao != '3' && opcao != '0'){
+		if(opcao != '1' && opcao != '2' && opcao != '3' && opcao != '4' && opcao != '0'){
 			printf("\n\t\t\t\t\t\t  OPCAO INVALIDA!");
 			Sleep(800);
 		}
@@ -146,7 +177,7 @@ void menu_organizadores(){
 				printf("\n\t\t\t\t\t\t\tBACKUP DE DADOS");
 				printf("\n");
 				backup();
-				printf("PRESSIONE UMA TECLA PARA CONTINUAR...");
+				printf("\n\tPRESSIONE UMA TECLA PARA CONTINUAR...");
 				getch();
 				break;
 			case '3':
@@ -154,11 +185,15 @@ void menu_organizadores(){
 				printf("\n\t\t\t\t\t\t\tRESTAURAR DADOS");
 				printf("\n");
 				restore();
-				printf("PRESSIONE UMA TECLA PARA CONTINUAR...");
+				printf("\n\tPRESSIONE UMA TECLA PARA CONTINUAR...");
 				getch();
-				break;		
+				break;
+			case '4':
+				main();
+				break;	
 		}
 	}while(opcao != '0');
+	fclose(arqEMP);
 }
 
 void menu_patrocinadores() {
@@ -168,23 +203,23 @@ void menu_patrocinadores() {
 void stands(Stands tipo){
 	system("cls");
 	printf("\n\t\t\t\t\t\t\tSTANDS");
-	printf("\nPRESSIONE UMA TECLA PARA CONTINUAR...");
+	printf("\n\tPRESSIONE UMA TECLA PARA CONTINUAR...");
 	getch();
 }
 
 void backup(){
 
-} 
+}
 
 void restore(){
-	
+
 }
 
 void vagas(){
 	system("cls");
 	printf("\n\t\t\t\t\t\t\tVAGAS");
 	printf("\n");
-	printf("PRESSIONE UMA TECLA PARA CONTINUAR...");
+	printf("\n\tPRESSIONE UMA TECLA PARA CONTINUAR...");
 	getch();
 }
 
@@ -192,146 +227,118 @@ void sobre(){
 	system("cls");
 	printf("\n\t\t\t\t\t\t\tSOBRE O EVENTO");
 	printf("\n");
-	printf("PRESSIONE UMA TECLA PARA CONTINUAR...");
+	printf("\n\tPRESSIONE UMA TECLA PARA CONTINUAR...");
 	getch();
 }
 
 void cad_participantes(Stands tipo){
 	gamers participante;
 	FILE *arqPAR;
-	char email[150];
+	char email[150], nome[100];
 	int flag = 0,cheio = 0;
-	
+
 	system("cls");
 	arqPAR = fopen("cadastro_partcipante.dat", "ab+");
 	do{
 		printf("\n\t\t\t\t\t\t\tCADASTRAR");
 		printf("\n");
-		printf("\nBEM-VINDO, GAMER. DIGITE SEU NOME>> ");
+		printf("\n\t%c BEM-VINDO, GAMER. DIGITE SEU NOME %c ", 254, 175);
 		fflush(stdin);
 		gets(participante.nome);
-		while(fread(&participante, sizeof(participante), 1, arqPAR)){
-			system("cls");
-			printf("\nOLA %s", participante.nome, " GAMER");
-			do{
-				printf("\nENDERECO DE E-MAIL>> ");
-				fflush(stdin);
-				gets(email);
+		strcpy(nome, participante.nome);
+		printf("\n\t%c OLA, %s", 254, nome, ".");
+		do{
+			printf("\n\t%c ENDERECO DE E-MAIL %c ", 254, 175);
+			fflush(stdin);
+			gets(email);
+			while(fread(&participante, sizeof(participante), 1, arqPAR)){
 				if(strcmp(email, participante.email) == 0){
-					printf("\nE-MAIL JA CADASTRADO NO SISTEMA, TENTE NOVAMENTE!");
+					printf("\n\tE-MAIL JA CADASTRADO NO SISTEMA, TENTE NOVAMENTE!");
 					flag = 1;
 				}
-			}while(flag == 1);
-		}
+			}
+		}while(flag == 1);
 		strcpy(participante.email, email);
 		do{
 			stands(tipo);
-			printf("\nESCOLHA O STAND DE JOGO QUE DESEJA PARTICIPAR>> ");
-			getch (participante.tipo_stand);
-			participante.tipo_stand = toupper(participante.tipo_stand);
-			switch(participante.tipo_stand){
+			printf("\n\n\t%c ESCOLHA O STAND DE JOGO QUE DESEJA PARTICIPAR %c ", 254, 175);
+			participante.stand = getchar();
+			participante.stand = toupper(participante.stand);
+		}while(participante.stand != 'R' && participante.stand != 'F' && participante.stand != 'A' && participante.stand != 'E' && participante.stand != 'L' && participante.stand != 'C');	
+			switch(participante.stand){
 				case 'R':
-					if(tipo.totalRPG < 10)
-						tipo.totalRPG++;
-					else{
-						printf("\nLIMITE DE VAGAS ATINGIDO!");
-						Sleep(500);
-						cheio = 1;	
-					}
+					
 					break;
 				case 'F':
-					if(tipo.totalFPS < 10)
-						tipo.totalFPS++;
-					else{
-						printf("\nLIMITE DE VAGAS ATINGIDO!");
-						Sleep(500);
-						cheio = 1;	
-					}	
+				
 					break;
 				case 'C':
-					if(tipo.totalCOR < 10)
-						tipo.totalCOR++;
-					else{
-						printf("\nLIMITE DE VAGAS ATINGIDO!");
-						Sleep(500);
-						cheio = 1;
-					}
+					
 					break;
 				case 'L':
-					if(tipo.totalLUT < 10)
-						tipo.totalLUT++;
-					else{
-						printf("\nLIMITE DE VAGAS ATINGIDO!");
-						Sleep(500);
-						cheio = 1;
-					}
+					
 					break;
 				case 'E':
-					if(tipo.totalESP < 10)
-						tipo.totalESP++;
-					else{
-						printf("\nLIMITE DE VAGAS ATINGIDO!");
-						Sleep(500);
-						cheio = 1;
-					}
+					
 					break;
 				case 'A':
-					if(tipo.totalAVE < 10)
-						tipo.totalAVE++;
-					else{
-						printf("\nLIMITE DE VAGAS ATINGIDO PARA O ESTILO AVENTURA ATINGIDO!");
-						Sleep(500);
-						cheio = 1;	
-					}
-					break;						
+					
+					break;
 			}
-		}while(cheio == 1);
 		fwrite(&participante, sizeof(participante), 1, arqPAR);
 		fclose(arqPAR);
-		printf("\nCADASTRO REALIZADO COM SUCESSO!");
-		Sleep(500);
+		printf("\n\tCADASTRO REALIZADO COM SUCESSO!");
+		Sleep(1000);
 		menu_participantes();
-	}while(tipo.totalRPG <= 10);	
+	}while(cheio == 1);
 }
 
-int cad_empresas(){
+void cad_empresas(){
 	FILE *arqEMP;
 	organizadores empresa;
-	int senha_cadastrada;
-	printf("\nNOME DA EMPRESA ORGANIZADORA>>>>>>>>>>>>>>>> ");
+	arqEMP = fopen("organizadores.dat", "ab");
+	printf("\n\t%c NOME DA EMPRESA ORGANIZADORA %c ", 254, 175);
 	fflush(stdin);
 	gets(empresa.nome);
-	printf("\nE-MAIL>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+	printf("\n\t%c E-MAIL %c ", 254, 175);
 	fflush(stdin);
 	gets(empresa.email);
-	printf("\nSENHA DE ACESSO PARA INFORMACOES DO EVENTO>> %d", senha_cadastrada);
+	empresa.senha = 1414;
+	printf("\n\t%c SENHA DE ACESSO PARA INFORMACOES DO EVENTO %c %d", 254, 175, empresa.senha);
+	fwrite(&empresa, sizeof(empresa), 1, arqEMP);
+	fclose(arqEMP);
 	getch();
-	return senha_cadastrada;
 }
 
 void cad_patrocinadores(){
+	
 	FILE *arqPAT;
 }
 
-void leValidaEmail(){
+void acessar(){
 	FILE *arqPAR;
-	int flag = 0;
 	gamers participante;
+	int flag = 0;
 	char email[150];
 	
-	arqPAR = fopen("cadastro_partcipante.dat", "ab+");
-	while(fread(&participante, sizeof(participante), 1, arqPAR)){
-			do{
-				printf("\nENDEREวO DE E-MAIL>> ");
-				fflush(stdin);
-				gets(email);
-				if(strcmp(email, participante.email) > 0){
-					printf("\nE-MAIL Jม CADASTRADO NO SISTEMA, TENTE NOVAMENTE!");
-					flag = 1;
-				}
-			}while(flag == 1);
-		strcpy(participante.email, email);
-	}
-	fwrite(&participante, sizeof(participante), 1, arqPAR);
+	arqPAR = fopen("cadastro_partcipante.dat", "rb");
+	system("cls");
+	do{
+		printf("\n\t%c DIGITE SEU EMAIL %c ", 254, 175);
+		fflush(stdin);
+		gets(email);
+		while(fread(&participante, sizeof(participante), 1, arqPAR)){
+			if(strcmp(email, participante.email) > 0){
+				printf("\n\t%c NOME %c %s", 254, 175, participante.nome);
+				printf("\n\t%c TIPO DE STAND %c %c", 254, 175, participante.stand);
+			}else{
+				flag = 1;
+			}
+		}
+		if(flag == 1)
+			printf("\n\tE-MAIL NAO CADASTRADO NO SISTEMA, TENTE NOVAMENTE!");
+		getch();	
+		menu_participantes();	
+	}while(flag == 1);
 	fclose(arqPAR);
 }
